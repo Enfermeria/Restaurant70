165e09b20d0daf58deaa096e433d1dd667593461
John David Molina <jd_v@yahoo.com>
2023-10-02 18:12:56 -0300

    Carga inicial

---
+++ Restaurant70/src/restaurant70/Restaurant70.java
@@ -0,0 +1,26 @@
+/*
+ 	Trabajo práctico final de la Guía 6 del curso Desarrollo de Apps
+	Universidad de La Punta en el marco del proyecto Argentina Programa 4.0
+
+	Integrantes:
+		John David Molina Velarde
+		Leticia Mores
+		Enrique Germán Martínez
+		Carlos Eduardo Beltrán
+ */
+package restaurant70;
+
+/**
+ *
+ * @author John David Molina Velarde, Leticia Mores, Enrique Germán Martínez, Carlos Eduardo Beltrán
+ */
+public class Restaurant70 {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO code application logic here
+	}
+	
+}
