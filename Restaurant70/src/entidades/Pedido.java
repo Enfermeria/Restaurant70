459335a59843a03d56abdcb2e8ea78d59aee1357
Quique <44074048+engema@users.noreply.github.com>
2023-10-11 17:28:19 -0300

    Esperemos que todo bien

---
+++ Restaurant70/src/entidades/Pedido.java
@@ -0,0 +1,92 @@
+/*
+	Trabajo práctico final de la Guía 6 del curso Desarrollo de Apps
+	Universidad de La Punta en el marco del proyecto Argentina Programa 4.0
+
+	Integrantes:
+		John David Molina Velarde
+		Leticia Mores
+		Enrique Germán Martínez
+		Carlos Eduardo Beltrán
+ */
+package entidades;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.Calendar;
+/**
+ *
+ * @author John David Molina Velarde, Leticia Mores, Enrique Germán Martínez, Carlos Eduardo Beltrán
+ */
+public class Pedido {
+	private int idPedido, idMesa, idMesero;
+        private  LocalDateTime fechaHora;
+	private boolean pagado;
+
+	public Pedido() {
+	}
+
+	public Pedido(int idMesa, int idMesero, LocalDateTime fechaHora, boolean pagado) {
+		this.idMesa = idMesa;
+		this.idMesero = idMesero;
+        this.fechaHora = fechaHora;
+		this.pagado = pagado;
+	}
+
+	public Pedido(int idPedido, int idMesa, int idMesero, java.time.LocalDateTime fechaHora, boolean pagado) {
+		this.idPedido = idPedido;
+		this.idMesa = idMesa;
+		this.idMesero = idMesero;
+		this.fechaHora = fechaHora;
+		this.pagado = pagado;
+}
+
+    	public int getIdPedido() {
+		return idPedido;
+	}
+
+	public void setIdPedido(int idPedido) {
+		this.idPedido = idPedido;
+	}
+
+	public int getIdMesa() {
+		return idMesa;
+	}
+
+	public void setIdMesa(int idMesa) {
+		this.idMesa = idMesa;
+	}
+
+	public int getIdMesero() {
+		return idMesero;
+	}
+
+	public void setIdMesero(int idMesero) {
+		this.idMesero = idMesero;
+	}
+
+	public LocalDateTime getFechaHora() {
+        return fechaHora;
+    }
+
+	public void setFechaHora(LocalDateTime fechaHora) {
+		this.fechaHora = fechaHora;
+	}
+
+	public boolean getPagado() {
+		return pagado;
+	}
+
+	public void setPagado(boolean pagado) {
+		this.pagado = pagado;
+	}
+
+	@Override
+	public String toString() {
+            
+		return "Pedido{" + "idPedido=" + idPedido + ", idMesa=" + idMesa + ", idMesero=" + idMesero + ", fechaHora=" + fechaHora + ", pagado=" + pagado + '}';
+	}
+	
+	
+}
